c1780f7ef26e09295a74922d56db333504e1c850
Mikolaj <mikolaj@grid.gg>
2021-05-10 23:22:59 +0200

    Add edge case for different worlds

--- src/me/snoh666/manhunt/CompassTracker.java
+++ src/me/snoh666/manhunt/CompassTracker.java
@@ -1,10 +1,12 @@
 package me.snoh666.manhunt;
 
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class CompassTracker extends BukkitRunnable {
@@ -33,6 +35,9 @@ public class CompassTracker extends BukkitRunnable {
     }
 
     private UUID getClosestPlayer(Location loc, UUID exceptPlayerId) {
+        if (Objects.requireNonNull(loc.getWorld()).getEnvironment() != World.Environment.NORMAL) {
+            return null;
+        }
         UUID closestPlayer = null;
         double distanceToClosestPlayer = 0.0D;
         double xLoc = loc.getX();
