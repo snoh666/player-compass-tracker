22e1d9fae5a66aa0341c767c8a109ab7cda519d3
Mikolaj <mikolaj@grid.gg>
2021-05-10 23:07:56 +0200

    Add proper disabling and enabling handling

--- src/me/snoh666/manhunt/InitCommand.java
+++ src/me/snoh666/manhunt/InitCommand.java
@@ -1,14 +1,19 @@
 package me.snoh666.manhunt;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class InitCommand extends JavaPlugin {
 
+    private final BukkitRunnable CompassTracker = new CompassTracker(this);
+
     @Override
     public void onEnable() {
-        new CompassTracker(this).runTaskTimer(this, 0L, 20L);
+        this.CompassTracker.runTaskTimer(this, 0L, 20L);
     }
 
     @Override
-    public void onDisable() {}
+    public void onDisable() {
+        this.CompassTracker.cancel();
+    }
 }
