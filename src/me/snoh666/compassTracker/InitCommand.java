7c7db08100a25fcb66403dde423c6470ef8d0813
Mikolaj <mikolaj@grid.gg>
2021-05-12 20:26:46 +0200

    Remove useless import

--- src/me/snoh666/compassTracker/InitCommand.java
+++ src/me/snoh666/compassTracker/InitCommand.java
@@ -1,7 +1,6 @@
 package me.snoh666.compassTracker;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.HandlerList;
