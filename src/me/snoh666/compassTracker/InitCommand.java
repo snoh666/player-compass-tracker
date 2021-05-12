78fe21e303675ba1aedef20cccd28fe50bc9e0cc
Mikolaj <mikolaj@grid.gg>
2021-05-12 19:11:40 +0200

    ADd basic info command

--- src/me/snoh666/compassTracker/InitCommand.java
+++ src/me/snoh666/compassTracker/InitCommand.java
@@ -1,5 +1,8 @@
 package me.snoh666.compassTracker;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -12,8 +15,24 @@ public class InitCommand extends JavaPlugin {
         this.CompassTracker.runTaskTimer(this, 0L, 20L);
     }
 
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("compass")) {
+            if (args.length > 0) {
+                // detect arg
+            } else {
+                sender.sendMessage(this.createMessage("Commands works correctly"));
+            }
+        }
+        return false;
+    }
+
     @Override
     public void onDisable() {
         this.CompassTracker.cancel();
     }
+
+    private String createMessage(String msg) {
+        return ChatColor.BLUE + "[compass_tracker]: " + ChatColor.WHITE + msg;
+    }
+
 }
