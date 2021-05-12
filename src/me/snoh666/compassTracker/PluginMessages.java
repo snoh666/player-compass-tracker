8b2f8c8b417c4489604819e06df8a042c5cf08cc
Mikolaj <mikolaj@grid.gg>
2021-05-12 20:22:33 +0200

    Add main messages class

---
+++ src/me/snoh666/compassTracker/PluginMessages.java
@@ -0,0 +1,9 @@
+package me.snoh666.compassTracker;
+
+import org.bukkit.ChatColor;
+
+public class PluginMessages {
+    public String createMessage(String msg) {
+        return ChatColor.BLUE + "[compass_tracker]: " + ChatColor.WHITE + msg;
+    }
+}
